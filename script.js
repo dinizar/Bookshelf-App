9dfdcde3ed44fc4fb7e31c16ba7d4bb60cc0717e
dinizar <dinizarafrian@gmail.com>
2022-07-16 13:13:13 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function openMenu(evt, menu) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(menu).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
